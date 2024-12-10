bc68d9b0f9b80637d806e4c261dec85b3c14b4c2
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-12-10 17:43:21 +0100

    completed context api exercise

--- src/context/TaskContextProvider.jsx
+++ src/context/TaskContextProvider.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TaskContext } from "./TaskContext";
 
 const TaskContextProvider = ({ children }) => {
-    const [tasksCompleted, setTasksCompleted] = useState(0); // Initialize task state
+    const [tasksCompleted, setTasksCompleted] = useState(0);
 
     return (
         <TaskContext.Provider value={{ tasksCompleted, setTasksCompleted }}>
